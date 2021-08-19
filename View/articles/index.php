bd42bf22bb5ed1e2008c64b3e9d225af893e3f15
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-08-19 16:58:37 +0200

    Changes by annaikoki

--- View/articles/index.php
+++ View/articles/index.php
@@ -2,6 +2,7 @@
 
 <?php // Use any data loaded in the controller here ?>
 
+
 <section>
     <p><a href="index.php">To home</a></p>
 
@@ -9,6 +10,8 @@
         <?php foreach ($articles as $article) : ?>
             <li><a href="<?= 'index.php?page=show&title='. $article->title?>"> <?= $article->title ?> - <?= $article->formatPublishDate('DD-MM-YYYY') ?> </a></li>
         <?php endforeach; ?>
+
+
     </ul>
 </section>
 
--- View/articles/show.php
+++ View/articles/show.php
@@ -1,1 +1,27 @@
 <?php // This page can be used as a detail page ?>
+
+<?php require 'View/includes/header.php'?>
+
+<?php // Use any data loaded in the controller here
+ ?>
+
+<section>
+    <p><a href="index.php">To home</a></p>
+
+    <?php $selectedTitle=$_GET['title'];
+//    var_dump($selectedTitle);
+    ?>
+
+    <h1><?= $selectedTitle; ?></h1>
+
+    <?php foreach ($articles as $article) : ?>
+<!--        --><?//= var_dump($articles); ?>
+        <?php if ($selectedTitle == $article->title){ ?>
+            <p><?= $article->description ?></p>
+        <?php  } ?>
+    <?php endforeach; ?>
+
+
+</section>
+
+<?php require 'View/includes/footer.php'?>
